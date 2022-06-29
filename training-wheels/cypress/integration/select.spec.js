a37a4319f2e58b596616b2c8946e0392f85bcf0c
Egberto Mendonça <egbertomendonca@gmail.com>
2022-06-29 14:44:32 -0300

    mais aprendizado de manipulacao de elementos

---
+++ training-wheels/cypress/integration/select.spec.js
@@ -0,0 +1,17 @@
+
+it('selecionar bucky por id', () => {
+    cy.visit('/select')
+
+    cy.get('#avengerList')
+        .select('Bucky')
+        .should('have.value', '2')
+});
+
+it('selecionar tony stark sem ser pelo id', () => {
+    cy.visit('/select')
+
+    cy.contains('option','Selecione um personagem')
+        .parent()
+        .select('Tony Stark')
+        .should('have.value', '3')
+});
---
+++ training-wheels/cypress/integration/timeout.spec.js
@@ -0,0 +1,12 @@
+
+it('exemplo de timeout', () => {
+    cy.visit('/timeout')
+
+    cy.contains('button', 'Habilita').click()
+
+    // cy.wait(7000)
+
+    cy.get('#firstname')
+        .should('be.visible')
+        .type('fernando', {timeout:7000})
+});
