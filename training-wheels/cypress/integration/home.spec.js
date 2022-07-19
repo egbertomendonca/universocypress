e68c270ed2428840b517fad3d84507981140c5d4
Egberto Mendonça <egbertomendonca@gmail.com>
2022-07-19 12:21:59 -0300

    automacao login cypress

--- training-wheels/cypress/integration/home.spec.js
+++
@@ -1,7 +0,0 @@
-it('deve exibir a home page', () => {
-    cy.visit('/')
-    cy.title()
-        .should('eq', 'Training Wheels | QAninja')
-    cy.get('ul[class=menu-list]')
-        .should('be.visible')
-});
--- training-wheels/cypress/integration/radio.spec.js
+++
@@ -1,7 +0,0 @@
-it('deve marcar thor ragnarok', () => {
-    cy.visit('/radios')
-    cy.get('input[value=thor3]')
-        .click()
-        .should('be.checked')
-
-});
